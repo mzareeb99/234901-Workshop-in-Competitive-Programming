62d126c1a2b5e817182bbe0df53f16866400ea1a
mzareeb <67233375+mzareeb99@users.noreply.github.com>
2022-05-24 19:28:12 +0300

    Update more10.cpp

--- HW1/code/more10.cpp
+++ HW1/code/more10.cpp
@@ -1,10 +1,54 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <unordered_map>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
 #include <string>
-
+#include <vector>
+#include <numeric>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<vi> v2i;
+typedef priority_queue<int> max_iheap;
+typedef priority_queue<ll> max_llheap;
+typedef priority_queue<ull> max_ullheap;
+typedef priority_queue<int, vi, greater<int>> min_iheap;
+typedef priority_queue<int, vll, greater<ll>> min_llheap;
+typedef priority_queue<int, vull, greater<ull>> min_ullheap;
+typedef vector<pair<double,double>> vpd;
+typedef pair<int,int> ii;
+typedef pair<double,double> dd;
+typedef pair<dd,double> ddd;
+typedef pair<int,ii> iii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef vector<si> vsi;
+typedef vector< vector<int> > vvi; // unweighted graph
+typedef vector< vector< pair<int,int> > > vvii; // weighted graph
+using namespace std;
+
+
+map<string, int> ver;
+vb act(1, false);
+vi suff[3];
+vii _not;
+
 struct unionfind {
     int n, c;
     vector<int> par, rank;
@@ -30,76 +74,72 @@ struct unionfind {
     }
 };
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
 
-    int N;
-    cin >> N;
 
-    unionfind uf(200000);
+bool checkIfIsRyh(unionfind& uf){
+    bool ret = true;
+    for (int i = 0; i < _not.size() && ret; ++i) {
+        ret = ret && !uf.same(_not[i].first, _not[i].second);
+    }
+    return ret;
+}
 
-    int V = 1;
-    unordered_map<string, int> vertex;
-    vector<bool> active(1, false);
-    vector<int> suff[3];
-    vector<pair<int, int> > NOT;
-    suff[0].push_back(-1); suff[1].push_back(-1); suff[2].push_back(-1);
+int aux(int N,unionfind& uf){
+    int num=1;
     string str[2], is, temp;
-    for (int ai = 0; ai < N; ++ai) {
+    for (int k = 0; k < N; ++k) {
         cin >> str[0] >> is >> str[1];
         int str_id[2];
         for (int x = 0; x < 2; ++x) {
             string &lhs = str[x];
-            auto it = vertex.find(lhs);
-            if (it == vertex.end()) {
-                vertex.insert({lhs, V});
-                active.push_back(true);
-                str_id[x] = V;
-                for (int i = 0; i < 3; ++i)
-                    suff[i].push_back(-1);
+            if (ver.find(lhs) == ver.end()) {
+                ver[lhs]= num;
+                act.push_back(true);
+                str_id[x] = num;
+                for (auto & i : suff)
+                    i.push_back(-1);
                 uf.add();
-                V++;
+                num++;
             } else {
-                str_id[x] = it->second;
-                active[str_id[x]] = true;
+                str_id[x] = ver[lhs];
+                act[str_id[x]] = true;
             }
             for (int L = 3; L > 0; --L) {
                 if (lhs.length() > L) {
                     temp = lhs.substr(lhs.length() - L);
-                    int ID = -1;
-                    auto it2 = vertex.find(temp);
-                    if (it2 == vertex.end()) {
-                        ID = V;
-                        vertex.insert({temp, V});
-                        active.push_back(L == 3);
-                        for (int i = 0; i < 3; ++i)
-                            suff[i].push_back(-1);
+                    int id;
+                    if (ver.find(temp) == ver.end()) {
+                        id = num;
+                        ver[temp]= num;
+                        act.push_back(L == 3);
+                        for (auto & i : suff)
+                            i.push_back(-1);
                         uf.add();
-                        V++;
-                    } else ID = it2->second;
-                    suff[L - 1][str_id[x]] = ID;
+                        num++;
+                    } else id = ver[temp];
+                    suff[L - 1][str_id[x]] = id;
                 }
             }
         }
         if (is == "is") uf.Union(str_id[0], str_id[1]);
-        else NOT.push_back({str_id[0], str_id[1]});
+        else _not.push_back({str_id[0], str_id[1]});
     }
+    return num;
+}
 
-    // Resolve suffixes
+
+
+int main() {
+    int N;
+    cin >> N;
+    unionfind uf(200000);
+    suff[0]={-1}; suff[1]={-1}; suff[2]={-1};
+    int V = aux(N,uf);
     for (int i = 0; i < V; ++i) {
-        for (int j = 0; j < 3; ++j)
-            if (suff[j][i] == -1) continue;
-            else if (!active[suff[j][i]]) continue;
-            else uf.Union(suff[j][i], i);
+        for (auto & j : suff)
+             if (j[i] == -1||!act[j[i]]) continue;
+            else uf.Union(j[i], i);
     }
-
-    bool yes = true;
-    for (int i = 0; i < NOT.size() && yes; ++i) {
-        yes = yes && !uf.same(NOT[i].first, NOT[i].second);
-    }
-
-    cout << (yes ? "yes" : "wait what?") << endl;
-
+    cout << (checkIfIsRyh(uf) ? "yes" : "wait what?") << endl;
     return 0;
 }
